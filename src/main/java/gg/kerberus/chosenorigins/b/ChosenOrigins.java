8db3225d37abb59d65e8e7d60d8d7b585167b916
MikhailTapio <mikhailtapio13468@163.com>
2023-04-19 11:03:24 +0800

    Initialization

---
+++ src/main/java/gg/kerberus/chosenorigins/b/ChosenOrigins.java
@@ -0,0 +1,10 @@
+package gg.kerberus.chosenorigins.b;
+
+import net.fabricmc.api.ModInitializer;
+
+public class ChosenOrigins implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
